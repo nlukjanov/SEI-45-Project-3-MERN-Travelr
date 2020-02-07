939b39ae18de60c92134c2b8c60da2e8b5135c8c
Nikita Lukjanov <nlukjanov@gmail.com>
2020-02-07 16:19:39 +0000

    added register controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,14 +4,13 @@ const { secret } = require('../config/environment')
 
 function register(req, res) {
   console.log(req.body)
-  res.json('ok')
-  // User.create(req.body)
-  //   .then(user =>
-  //     res
-  //       .status(201)
-  //       .json({ message: `Thank you for registering ${user.name}` })
-  //   )
-  //   .catch(err => res.json(err))
+  User.create(req.body)
+    .then(user =>
+      res
+        .status(201)
+        .json({ message: `Thank you for registering ${user.name}` })
+    )
+    .catch(err => res.json(err))
 }
 
 module.exports = { register }
