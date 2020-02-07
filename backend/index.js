77ac80e1a4b2fb3ade6b437ae7d6f4e2f9c1a17e
Nikita Lukjanov <nlukjanov@gmail.com>
2020-02-07 16:19:28 +0000

    added router

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const { port, dbURI } = require('./config/environment')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const logger = require('./lib/logger')
-// const router = require('./config/router')
+const router = require('./config/router')
 
 mongoose.connect(
   dbURI,
@@ -17,6 +17,8 @@ mongoose.connect(
 
 app.use(bodyParser.json())
 app.use(logger)
+
+app.use(router)
 
 app.use('/*', (req, res) => res.json({ message: 'hello' }))
 
