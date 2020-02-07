78953bbb55334e6ac547134a652e5bb7a3e9c791
bheki-maenetja <bhekimaenetja@gmail.com>
2020-02-07 17:31:56 +0000

    added group controller

--- backend/config/router.js
+++ backend/config/router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const users = require('../controllers/authController')
+const groups = require('../controllers/groupController')
 
 router.route('/register').post(users.register)
 
