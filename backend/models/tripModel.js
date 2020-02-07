b8d3dac1e972c169d07e88da3a9ac14250497a83
ZihniCanKalsin <ckalsin@hotmail.com>
2020-02-07 16:35:02 +0000

    trips controller v1

--- backend/models/tripModel.js
+++ backend/models/tripModel.js
@@ -2,17 +2,15 @@ const mongoose = require('mongoose')
 
 
 const tripSchema = new mongoose.Schema({
-  organizer: { type: mongoose.Schema.ObjectId, ref:'User', required: true },
+  organizer: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
   country: { type: String, required: true },
   startingDate: { type: Date, required: true },
   endingDate: { type: Date, required: true },
   category: { type: mongoose.Schema.ObjectId, ref: 'Category', required: true },
   description: { type: String, required: true },
   participants: [{ type: mongoose.Schema.ObjectId, ref: 'User' }],
-  budget: { type: String, required: true }
-
-
-  
+  budget: { type: String, required: true },
+  interested: [{ type: mongoose.Schema.ObjectId, ref: 'User' }]
 })
 
 module.exports = mongoose.model('Trip', tripSchema)
