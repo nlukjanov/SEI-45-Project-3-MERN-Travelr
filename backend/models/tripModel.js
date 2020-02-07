c0a3e11ad3d61f4013d45e08dc5e1e5b01c71f95
ZihniCanKalsin <ckalsin@hotmail.com>
2020-02-07 15:34:56 +0000

    createad trip model

---
+++ backend/models/tripModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+
+const tripSchema = new mongoose.Schema({
+  organizer: { type: mongoose.Schema.ObjectId, ref:'User', required: true },
+  country: { type: String, required: true },
+  startingDate: { type: Date, required: true },
+  endingDate: { type: Date, required: true },
+  category: { type: mongoose.Schema.ObjectId, ref: 'Category', required: true },
+  description: { type: String, required: true },
+  participants: [{ type: mongoose.Schema.ObjectId, ref: 'User' }],
+  budget: { type: String, required: true }
+
+
+  
+})
+
+module.exports = mongoose.model('Trip', tripSchema)
