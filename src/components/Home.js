d91198b35047184f01f3d7307567a988f05feaf7
Nikita Lukjanov <nlukjanov@gmail.com>
2020-02-10 14:59:14 +0000

    added tabs to home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,33 @@
 import React, { Component } from 'react'
-
-import moduleName from 'module'
+import TripsIndex from './TripsIndex'
+import GroupIndex from './GroupIndex'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
 
 class Home extends Component {
+  state = { tabIndex: 0 }
   render() {
     return (
       <section className='is-fullheight-with-navbar'>
-        <section className='hero is-small is-primary is-bold'>
+        <div className='hero is-medium is-primary is-bold'>
           <div className='hero-body'>
             <div className='container has-text-centered'>
               <h1 className='title'>Travelr</h1>
               <h2 className='subtitle'>Find your travel partner</h2>
             </div>
           </div>
-        </section>
-        <div className='tabs is-centered is-fullwidth'>
-          <ul>
-            <li className='is-active'>
-              <a>Trips</a>
-            </li>
-            <li>
-              <a>Travel Groups</a>
-            </li>
-          </ul>
+        </div>
+        <div>
+          <Tabs
+            selectedIndex={this.state.tabIndex}
+            onSelect={tabIndex => this.setState({ tabIndex })}
+          >
+            <TabList className='tabs is-fullwidth is-centered'>
+              <Tab>Trips</Tab>
+              <Tab>Groups</Tab>
+            </TabList>
+            <TabPanel><TripsIndex /></TabPanel>
+            <TabPanel><GroupIndex /></TabPanel>
+          </Tabs>
         </div>
       </section>
     )
