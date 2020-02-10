ef369a3257ed2e1b0a39b9bb42c3663b478a9d52
Nikita Lukjanov <nlukjanov@gmail.com>
2020-02-10 11:45:27 +0000

    added navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className='navbar'>
+        <div className='navbar-start'>
+          <Link className='navbar-item' to='/'>
+            Home
+          </Link>
+          <Link className='navbar-item' to='/trips/new'>
+            Create New Trip
+          </Link>
+          <Link className='navbar-item' to='/groups/new'>
+            Create New Group
+          </Link>
+        </div>
+        <div className='navbar-end'>
+          <Link className='navbar-item' to='/register'>
+            Register
+          </Link>
+          <Link className='navbar-item' to='/login'>
+            Login
+          </Link>
+          <Link className='navbar-item' to='/myaccount'>
+            My Account
+          </Link>
+        </div>
+      </nav>
+    )
+  }
+}
+
+export default Navbar
