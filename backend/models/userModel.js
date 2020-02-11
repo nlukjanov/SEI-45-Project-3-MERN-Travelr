32a0973c031c81cc91e40fdcdf1ae899a8cbee10
bheki-maenetja <bhekimaenetja@gmail.com>
2020-02-11 16:31:43 +0000

    refactored delete user controller

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -85,8 +85,4 @@ userSchema.pre('save', function hashPassword(next) {
   next()
 })
 
-userSchema.pre('remove', function(next) {
-  this.model('Group').remove({ 'members.user._id': this._id }, next)
-})
-
 module.exports = mongoose.model('User', userSchema)
