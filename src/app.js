8756a99bc93f28e3706e28e788e7c4cffe97aae3
Nikita Lukjanov <nlukjanov@gmail.com>
2020-02-12 19:28:46 +0000

    renamed secure route for front end

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ import ShowTrip from './components/ShowTrip'
 import EditTrip from './components/EditTrip'
 import NewGroup from './components/NewGroup'
 import EditGroup from './components/EditGroup'
-import SecureRoute from './lib/SecureRoute'
+import SecureRouteFront from './lib/SecureRouteFront'
 
 class App extends React.Component {
   render() {
@@ -24,14 +24,14 @@ class App extends React.Component {
           <Navbar />
           <Switch>
             <Route exact path='/' component={Home}></Route>
-            <SecureRoute path='/trips/:id/edit' component={EditTrip} />
-            <SecureRoute path='/trips/new' component={NewTrip} />
+            <SecureRouteFront path='/trips/:id/edit' component={EditTrip} />
+            <SecureRouteFront path='/trips/new' component={NewTrip} />
             <Route path='/trips/:id' component={ShowTrip}></Route>
-            <SecureRoute path='/groups/:id/edit' component={EditGroup} />
-            <SecureRoute path='/groups/new' component={NewGroup} />
+            <SecureRouteFront path='/groups/:id/edit' component={EditGroup} />
+            <SecureRouteFront path='/groups/new' component={NewGroup} />
             <Route path='/register' component={Register}></Route>
             <Route path='/login' component={Login}></Route>
-            <SecureRoute path='/myaccount' component={MyAccount} />
+            <SecureRouteFront path='/myaccount' component={MyAccount} />
           </Switch>
         </main>
       </BrowserRouter>
--- src/lib/SecureRouteFront.js
+++ src/lib/SecureRouteFront.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
 import Auth from './authHelper'
 
-const SecureRoute = ({ component: Component, ...rest }) => {
+const SecureRouteFront = ({ component: Component, ...rest }) => {
   if (Auth.isAuthenticated()) return <Route {...rest} component={Component} />
   Auth.logout()
   return <Redirect to='/login' />
 }
 
-export default SecureRoute
+export default SecureRouteFront
