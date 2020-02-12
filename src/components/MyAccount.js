84a6a4c378664157def894dbca111cfe4c4d5102
Nikita Lukjanov <nlukjanov@gmail.com>
2020-02-12 18:50:01 +0000

    added trips to my account page

--- src/components/MyAccount.js
+++ src/components/MyAccount.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
 import axios from 'axios'
 import Auth from '../lib/authHelper'
+import MyTripList from './MyTripsList'
+var moment = require('moment')
 
 class MyAccount extends Component {
   state = { tabIndex: 0, user: {} }
@@ -16,23 +18,41 @@ class MyAccount extends Component {
     })
   }
 
+  completedTrips = trips => {
+    if (!trips) return null
+    return trips.filter(trip => {
+      return Date.parse(trip.endingDate) < Date.now()
+    })
+  }
+
   render() {
-    const { dob, country, city, gender, languages } = this.state.user
-    console.log(this.state.user)
+    if (!this.state.user) return null
+    if (!this.state.user.dob) return null
+    const {
+      dob,
+      country,
+      city,
+      gender,
+      languages,
+      organizedTrips,
+      joinedTrips,
+      favoriteTrips
+    } = this.state.user
     return (
       <section className='is-fullheight-with-navbar'>
-        <div className='hero is-medium is-primary is-bold'>
-          <div className='hero-body'>
+        <div className='hero is-small is-primary is-bold'>
+          <div className='hero-body is-small'>
             <div className='columns'>
               <div className='column is-4'>
                 <figure className='image is-square'>
-                  <img src={this.state.user.profileImage} alt="profile image"/>
+                  <img src={this.state.user.profileImage} alt='profile image' />
                 </figure>
               </div>
               <div className='column is-7 is-offset-1'>
-                <div className='container'>{dob}</div>
-                <div className='container'>{country}</div> 
-                {/* add map to country */}
+                <div className='container'>
+                  {moment().diff(`${dob}`, 'years')}
+                </div>
+                <div className='container'>{country}</div>
                 <div className='container'>{city}</div>
                 <div className='container'>{gender}</div>
                 <div className='container'>Languages Spoken: {languages}</div>
@@ -46,15 +66,23 @@ class MyAccount extends Component {
             onSelect={tabIndex => this.setState({ tabIndex })}
           >
             <TabList className='tabs is-fullwidth is-centered'>
-              <Tab>Created Trips</Tab>
-              <Tab>Joined Trip</Tab>
-              <Tab>Trips Interested In</Tab>
-              <Tab>Completed Trips</Tab>
+              <Tab className='button'>Organized Trips</Tab>
+              <Tab className='button'>Joined Trip</Tab>
+              <Tab className='button'>Trips Interested In</Tab>
+              <Tab className='button'>Completed Trips</Tab>
             </TabList>
-            <TabPanel><JoinedTrips /></TabPanel>
-            <TabPanel>Tab 2</TabPanel>
-            <TabPanel>Tab 3</TabPanel>
-            <TabPanel>Tab 4</TabPanel>
+            <TabPanel>
+              <MyTripList data={organizedTrips} />
+            </TabPanel>
+            <TabPanel>
+              <MyTripList data={joinedTrips} />
+            </TabPanel>
+            <TabPanel>
+              <MyTripList data={favoriteTrips} />
+            </TabPanel>
+            <TabPanel>
+              <MyTripList data={this.completedTrips(organizedTrips)} />
+            </TabPanel>
           </Tabs>
         </div>
       </section>
