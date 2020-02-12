2017a5220263a337f05238f1cbe55c616f6c6b0e
Nikita Lukjanov <nlukjanov@gmail.com>
2020-02-12 19:10:31 +0000

    added SecureRoute on frontend

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ import ShowTrip from './components/ShowTrip'
 import EditTrip from './components/EditTrip'
 import NewGroup from './components/NewGroup'
 import EditGroup from './components/EditGroup'
-
+import SecureRoute from './lib/SecureRoute'
 
 class App extends React.Component {
   render() {
@@ -24,15 +24,14 @@ class App extends React.Component {
           <Navbar />
           <Switch>
             <Route exact path='/' component={Home}></Route>
-            <Route path='/trips/:id/edit' component={EditTrip} />
-            <Route exact path='/trips/new' component={NewTrip}></Route>
-            <Route exact path='/trips/:id' component={ShowTrip}></Route>
-            <Route path='/groups/:id/edit' component={EditGroup}></Route>
-            <Route path='/groups/new' component={NewGroup}></Route>
-            <Route path='/trips/:id' component={ShowTrip} />
-            <Route exact path='/auth/register' component={Register}></Route>
-            <Route exact path='/auth/login' component={Login}></Route>
-            <Route exact path='/myaccount' component={MyAccount}></Route>
+            <SecureRoute path='/trips/:id/edit' component={EditTrip} />
+            <SecureRoute path='/trips/new' component={NewTrip} />
+            <Route path='/trips/:id' component={ShowTrip}></Route>
+            <SecureRoute path='/groups/:id/edit' component={EditGroup} />
+            <SecureRoute path='/groups/new' component={NewGroup} />
+            <Route path='/register' component={Register}></Route>
+            <Route path='/login' component={Login}></Route>
+            <SecureRoute path='/myaccount' component={MyAccount} />
           </Switch>
         </main>
       </BrowserRouter>
