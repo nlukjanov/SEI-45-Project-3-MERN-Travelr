8403479172b2fd877f27dfc224ce3105606b77d5
Nikita Lukjanov <nlukjanov@gmail.com>
2020-02-25 18:28:54 +0000

    preparation for deployment

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const { port, dbURI } = require('./config/environment')
+const { port, dbURI } = require('./backend/config/environment')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-const logger = require('./lib/logger')
-const handleError = require('./lib/errorHandler')
-const router = require('./config/router')
+const logger = require('./backend/lib/logger')
+const handleError = require('./backend/lib/errorHandler')
+const router = require('./backend/config/router')
 
 mongoose.connect(
   dbURI,
